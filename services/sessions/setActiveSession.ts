572ba6fa3d4df1e639462ebd4d2c5c07a113c750
Andres Mercado <andres23.t30@gmail.com>
2026-07-09 11:27:01 -0500

    batucaaAAaando

--- services/sessions/setActiveSession.ts
+++ services/sessions/setActiveSession.ts
@@ -2,17 +2,16 @@ const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
 
 export async function setActiveSession(sessionId: string) {
-  const res = await fetch(`${API_URL}/sessions/active`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ sessionId }),
-  });
 
-  if (!res.ok) {
-    throw new Error(await res.text());
-  }
+    console.log("API_URL:", API_URL);
 
-  return await res.json();
+    const res = await fetch(`${API_URL}/sessions/active`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sessionId }),
+    });
+
+    return await res.json();
 }
